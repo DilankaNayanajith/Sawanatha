4dbd988510daa2e5a0e15e582a5d49d4c4374dbc
DilankaNayanajith <dilanka4nayanajith@gmail.com>
2019-06-19 15:59:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sawanatha/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.sawanatha;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityMored();
+
+            }
+        });
+    }
+
+    public void openActivityMored() {
+        Intent intent = new Intent(this, more_dActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Are you sure you want to exit?").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.super.onBackPressed();
+            }
+        }).setNegativeButton("Cancel",null);
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
---
+++ app/src/main/java/com/example/sawanatha/more_dActivity.java
@@ -0,0 +1,21 @@
+package com.example.sawanatha;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class more_dActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_d);
+
+
+    }
+
+}
+
